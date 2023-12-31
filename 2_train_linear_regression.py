ec7de90305a38988c53ab307d1903d5cc1de28d2
zuzanakf <114612717+zuzanakf@users.noreply.github.com>
2023-12-31 15:08:40 +0000

    final_arousal_mfccs

--- 2_train_linear_regression.py
+++ 2_train_linear_regression.py
@@ -10,90 +10,85 @@ import os
 import librosa
 
 numpy_directory = 'audio_data\\numpy_arrays'
+raw_audio_directory = 'audio_data\\raw'
 resampled_directory = 'audio_data\\numpy_arrays_resampled'
 target_sr = 44100  #taget 
 
 
-# Resample each audio file
-for filename in os.listdir(numpy_directory):
-    if filename.endswith('.npy'):
-        filepath = os.path.join(numpy_directory, filename)
-        array = np.load(filepath)
+# Iterate through each .npy file to find corresponding .wav files as not using some wavs due to large sr differences
+for npy_file in os.listdir(numpy_directory):
+    if npy_file.endswith('.npy'):
+        wav_filename = npy_file.replace('.npy', '.wav')
+        wav_filepath = os.path.join(raw_audio_directory, wav_filename)
 
-        #resample audio to target sampling rate
-        resampled_array = librosa.resample(array, orig_sr=librosa.get_samplerate(filepath), target_sr=target_sr)
+        if os.path.exists(wav_filepath):
+            #load the original audio file with its original sampling rate
+            y, orig_sr = librosa.load(wav_filepath, sr=None)
 
-        #save
-        resampled_path = os.path.join(resampled_directory, filename)
-        np.save(resampled_path, resampled_array)
+            #check if resampling needed
+            if orig_sr != target_sr:
+                # Resample the audio directly
+                y_resampled = librosa.resample(orig_sr=orig_sr, target_sr=target_sr, y=y)
+            else:
+                y_resampled = y
+
+            #save the resampled audio data as a new .npy file
+            resampled_filepath = os.path.join(resampled_directory, npy_file)
+            np.save(resampled_filepath, y_resampled)
 
 print("Resampling complete.")
+print(resampled_directory)
 
 def extract_mfccs(audio_array, sr=44100, n_mfcc=13):
     mfccs = librosa.feature.mfcc(y=audio_array, sr=sr, n_mfcc=n_mfcc)
     mfccs_mean = np.mean(mfccs, axis=1)
     return mfccs_mean
 
-# Prepare DataFrame for MFCCs
+
 data = []
 
 for filename in os.listdir(resampled_directory):
     if filename.endswith('.npy'):
         filepath = os.path.join(resampled_directory, filename)
         array = np.load(filepath)
-
         mfccs = extract_mfccs(array)
-        data.append({'filename': filename, 'mfccs': mfccs})
+        # Flatten MFCCs here
+        mfccs_flattened = mfccs.flatten()
+        data.append({'filename': filename.replace('.npy', '.wav'), 'mfccs': mfccs_flattened})
 
 mfcc_df = pd.DataFrame(data)
-#ensuring filename is the same
-mfcc_df['filename'] = mfcc_df['filename'].apply(lambda x: x.replace('.npy', '.wav'))
 
-# Load arousal ratings DataFrame
+#load arousal ratings DataFrame
 with open('emotion_data/dataframes/arousal_ratings.pkl', 'rb') as file:
     arousal_df = pickle.load(file)
 
-#checks
-print("Audio DataFrame Size:", mfcc_df.shape)
-print(mfcc_df.head())
-print("Arousal Ratings DataFrame Size:", arousal_df.shape)
-print(arousal_df.head())
-
-# Merge the MFCC DataFrame with the arousal ratings
+#Merge MFCC df with the arousal ratings
 merged_df = pd.merge(mfcc_df, arousal_df, on='filename')
 
-#checks
-print("Merged DataFrame Size:", merged_df.shape)
-print(merged_df.head())
-
-# Prepare data for regression
+#prepare data
 X = np.vstack(merged_df['mfccs'].values)
 y = merged_df['rating'].values
 
-# Split the data into training/testing sets
+#training/testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 #linear regression object
 regr = linear_model.LinearRegression()
 
-#train using the training sets
+#Train
 regr.fit(X_train, y_train)
 
-# Make predictions and evaluate the model
+#predictions
 y_pred = regr.predict(X_test)
 
-
-#coefficients
+#coefficients, mean squared error, and coefficient of determination
 print("Coefficients: \n", regr.coef_)
-#mean squared error
 print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
-#coefficient of determination: 1 is perfect prediction
 print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
 
-#Plot outputs (adjusting for the fact that X_test is multidimensional)
-plt.scatter(X_test[:, 0], y_test, color="black")#Plotting only the first feature of X_test for visualization
+# Since we can't easily plot high-dimensional data, consider alternative visualization or analysis methods
+# If you still want to plot, here is a simple scatter plot for the first feature
+plt.scatter(X_test[:, 0], y_test, color="black")
 plt.plot(X_test[:, 0], y_pred, color="blue", linewidth=3)
 
 plt.show()
-
-
